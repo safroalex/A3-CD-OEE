dec94943d43f50cf45c13d4d0cdcd501d6dab09a
George3005 <podshivalovg2002@gmail.com>
2024-02-22 16:05:53 +0300

    update of package and final install

--- install-docker.sh
+++ install-docker.sh
@@ -8,3 +8,6 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
