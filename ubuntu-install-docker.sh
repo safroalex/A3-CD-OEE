93a361a1fcbfad2bc06fc468488062ec3590e9d3
Fchuyproa11 <tthhhuy7@gmail.com>
2024-02-22 17:07:59 +0300

    link for installing docker gpg

---
+++ ubuntu-install-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
