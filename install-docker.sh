e0ae78c2f02f0d71354ec0192d6e8284d03598a7
George3005 <podshivalovg2002@gmail.com>
2024-02-22 16:04:34 +0300

    instructions for installing docker on debian

---
+++ install-docker.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+sudo apt-get update 
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
